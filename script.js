a3e16a60ca25f2961b655cde73d2e4f45ff65616
katya20 <kpanchenko21@gmail.com>
2016-10-21 08:45:38 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/addListItem("product-list", {name: "donut", picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg",  })
+addListItem("product-list", {name: "donut", picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", price: "5", rating: "10/10", weight: "0.5lb"})
 
 onNewListItem("product-list", function(product) {
   console.log(product)
