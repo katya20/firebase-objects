65094a213d3c6566c96cbaa96e1e548638689f80
katya20 <kpanchenko21@gmail.com>
2016-10-21 08:50:11 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 addListItem("product-list", {name: "donut", picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", price: "5", rating: "10/10", weight: "0.5lb"})
-addListItem("product-list", {name: "cool shoes", picture: "
+addListItem("product-list", {name: "cool shoes", picture: "http://oddstuffmagazine.com/wp-content/uploads/2011/09/productdesign_013.jpg", price: "264", rating: "0", weight: "5lb"})
 
 onNewListItem("product-list", function(product) {
   console.log(product)
