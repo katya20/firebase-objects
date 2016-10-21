aaf62a4ddc03fa6e00d5faf5aae900687e027a05
katya20 <kpanchenko21@gmail.com>
2016-10-21 08:52:04 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ onNewListItem("product-list", function(product) {
   console.log(product)
   var nameHeader = $("<h1 class='product-name'>" + product.name + "</h1>")
   var image = $("<img class='product-image'>").attr("src", product.picture)  
+//  var price = $("
 
   $(".product-list-container").append(nameHeader, image)
 })
